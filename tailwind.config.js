9da94536083a971c326b16f0de856c6a04adb5fe
Olayinka <theolayinkayodele@gmail.com>
2023-01-31 20:25:16 +0100

    starrts showcase section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lineGray : '#C4C4C4'
+        lineGray : '#C4C4C4',
+        pinkish : '#E9D1C9',
+        grayish : '#F1F0F0',
+        greyish : '#E0DED3'
       },
       fontFamily: {
         'oswald' : ['oswald'],
