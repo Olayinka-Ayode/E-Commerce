06775dd90b7988818daf9ccfd4341b24fe5aecbd
Olayinka <theolayinkayodele@gmail.com>
2023-03-01 23:16:22 +0100

    gen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html, ./*.js"],
   theme: {
     extend: {
       colors: {
         lineGray : '#C4C4C4',
         pinkish : '#E9D1C9',
         grayish : '#F1F0F0',
-        greyish : '#E0DED3'
+        greyish : '#E0DED3',
+        cat_gray : '#BDBDBD',
+        bg_bluey : '#E6F1FA'
       },
       fontFamily: {
         'oswald' : ['oswald'],
