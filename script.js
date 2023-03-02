d13f0ad2e24dc304924ad16e59b91088cb22c763
Olayinka <theolayinkayodele@gmail.com>
2023-03-02 23:45:21 +0100

    transfer

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ ml1 = document.getElementById('ham-1');
 ml2 = document.getElementById('ham-2');
 ml3 = document.getElementById('ham-3');
 
-menu.classList.toggle('active');
 
-for(let i = menu.classList; i > menu.classList; i++) {
-    if(i === 'active') {
-        ml1.classList.toggle('active')   
-        ml2.classList.toggle('active')   
-        ml3.classList.toggle('active')   
-    }
+function transform(){
+    menu.classList.toggle('active');
+    console.log('fwe')
+    console.log(menu.classList)
+    console.log(ml1.classList);
+    console.log(ml2.classList);
+    console.log(ml3.classList);
 }
 
-console.log(ml1.classList)
-console.log(menu.classList)
+for(i = 0; i < menu.classList.length; i++){
+    if(menu.classList.contains('active')) {
+            ml1.classList.toggle('active-1')
+            ml2.classList.toggle('active-2')
+            ml3.classList.toggle('active-3')
+        }
+}
+
+    
