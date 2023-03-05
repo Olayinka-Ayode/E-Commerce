4e38d43ed1484c1051569d5b9b3b8ab4f60c012a
Olayinka <theolayinkayodele@gmail.com>
2023-03-05 13:39:05 +0100

    hamburger menu works

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-menu = document.getElementById('ham');
-ml1 = document.getElementById('ham-1');
-ml2 = document.getElementById('ham-2');
-ml3 = document.getElementById('ham-3');
+const menu = document.getElementById('hamburger');
+const tops = document.getElementById('top');
+const mid = document.getElementById('mid')
+const bottom = document.getElementById('bottom')
 
-
-function transform(){
-    menu.classList.toggle('active');
-    console.log('fwe')
-    console.log(menu.classList)
-    console.log(ml1.classList);
-    console.log(ml2.classList);
-    console.log(ml3.classList);
+function toggleAnime() {
+    if(menu.classList.contains('active')){
+        console.log('hello')
+        tops.classList.toggle('active-1')
+        mid.classList.toggle('active-2')
+        bottom.classList.toggle('active-3')
+    }else{
+        menu.classList.toggle('active')
+    }
 }
 
-for(i = 0; i < menu.classList.length; i++){
-    if(menu.classList.contains('active')) {
-            ml1.classList.toggle('active-1')
-            ml2.classList.toggle('active-2')
-            ml3.classList.toggle('active-3')
-        }
-}
-
-    
+menu.addEventListener('click', toggleAnime);
